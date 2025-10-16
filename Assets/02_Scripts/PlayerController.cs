51a667980510e02640b5d884ffaed9ad66af3259
candiali <cel0035217@est.univalle.edu>
2025-10-16 19:25:18 -0400

    Animaciones basicas

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -13,10 +13,15 @@ public class PlayerController : MonoBehaviour
     private bool onGround;
     //
     private Rigidbody2D rb;
+
+    //animator
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,33 +29,41 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
         Jump();
-       
         
     }
     void Movement()
     {
-        float speedX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        //Changes the player orientation
-        if (speedX < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
+        //float speedX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float speedX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
-        }
-        if (speedX > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
+        //Changes the player orientation
+        Vector3 scale = transform.localScale;
+        if (speedX < 0)
+            scale.x = -Mathf.Abs(scale.x);
+        else if (speedX > 0)
+            scale.x = Mathf.Abs(scale.x);
+
+        transform.localScale = scale;
+
+        //Animation
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        //Move
         Vector3 position = transform.position;
-        transform.position = new Vector3(speedX + position.x, position.y, position.z);
+        transform.position = new Vector3(position.x + speedX, position.y, position.z);
     }
+
     void Jump()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastLenght, layerFloor);
         onGround = hit.collider != null;
+
         if (onGround && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
+
+        animator.SetBool("IsGrounded", onGround);
 
     }
     private void OnDrawGizmos()
