23780d01556771969539345534e2b97e997e173b
candiali <cel0035217@est.univalle.edu>
2025-10-22 23:23:10 -0400

    SpritesPersonajes

---
+++ Assets/02_Scripts/Characters/UnoPintacarita.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnoPintacarita : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator.Play("UnoPintacarita");
+    }
+}
