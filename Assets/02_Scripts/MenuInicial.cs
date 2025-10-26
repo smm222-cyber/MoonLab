d43a2b04b45297ff5f724133e41315ae6ed9c6c7
VictoriaGuerra <gbv0035239@est.univalle.edu>
2025-10-26 19:05:22 -0400

    Menu/Tutoral

---
+++ Assets/02_Scripts/MenuInicial.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   
+    public void salir()
+    {
+        Debug.Log("Salir..");
+        Application.Quit();
+    }
+}
