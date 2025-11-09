a1f31a82180a3f82271e6c98c49460735837001e
Nayara Hurtado <hbn0035194@est.univalle.edu>
2025-11-08 21:58:45 -0400

    Push con el merge

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -70,7 +70,6 @@ public class GameManager : MonoBehaviour
     // Diccionario que relaciona cada día con el nombre real de la escena
     private Dictionary<int, string> dayToScene = new Dictionary<int, string>()
     {
-        //Poner los siguientes niveles para que lo lea
         {1, "Tutorial"},
         {2, "Pintacaritas_Level"},
         {3, "Vendedor_Level"} // si más adelante tienes más días
@@ -119,7 +118,7 @@ public class GameManager : MonoBehaviour
         {
             HandleDialogClick();
         }
-        //Detecta cuando el cuadro de texto esta activo
+
         if (textBox != null && textBox.activeSelf && Input.GetMouseButtonDown(0))
         {
             CloseNonCollectableText();
